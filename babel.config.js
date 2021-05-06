c6ad840710568be37393b4ae53d09ca0209635a6
Alex Freska <alex.freska@gmail.com>
2021-05-06 13:57:13 -0400

    Remove babels

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,3 @@
 module.exports = {
-  presets: [
-    '@babel/preset-env',
-    '@babel/preset-typescript',
-    '@babel/preset-react',
-  ],
-  plugins: ['@babel/plugin-transform-runtime'],
+  presets: ['@babel/preset-typescript', '@babel/preset-react'],
 }

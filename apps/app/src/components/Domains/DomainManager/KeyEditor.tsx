0c70284b48808b20275600d0b1eb2ab98c2140c7
Alex Freska <alex.freska@gmail.com>
2021-08-24 15:04:39 -0400

    Format

--- apps/app/src/components/Domains/DomainManager/KeyEditor.tsx
+++ apps/app/src/components/Domains/DomainManager/KeyEditor.tsx
@@ -104,10 +104,10 @@ export function KeyEditor({ domain, dataKey }: Props) {
     setEditingValue(value)
   }, [setEditingValue, value])
 
-  const isDataLatest = useMemo(
-    () => value === editingValue,
-    [value, editingValue]
-  )
+  const isDataLatest = useMemo(() => value === editingValue, [
+    value,
+    editingValue,
+  ])
 
   const isValid = useMemo(() => {
     try {
